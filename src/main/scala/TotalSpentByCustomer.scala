d7938b4c522ad9450ef0df536dd98679f89a4ce5
agustin.delosriscos <agustin.delosriscos@kairosds.com>
2021-08-13 19:32:06 +0200

    Section 3.10 - Get total spent by customer on my own

---
+++ src/main/scala/TotalSpentByCustomer.scala
@@ -0,0 +1,32 @@
+import org.apache.spark._
+import org.apache.log4j.{Logger, Level}
+
+object TotalSpentByCustomer {
+
+  def parseLines(line: String): (Int, Float) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
+  }
+  def main(args: Array[String]): Unit = {
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "TotalSpentByCustomer")
+
+    // Do the magic
+    val lines = sc.textFile("./data/customer-orders.csv")
+    val fields = lines.map(parseLines)
+    val countByCustomer = fields.reduceByKey(_ + _)
+    val countByCustomerSorted = countByCustomer.map(x => (x._2, x._1)).sortByKey(ascending = false)
+    val results = countByCustomerSorted.collect()
+
+    // Print first ten clients that spent more
+    for( result <- results.take(10)) {
+      val client = result._2
+      val count = result._1
+      println(s"$client, $count")
+    }
+  }
+
+}
