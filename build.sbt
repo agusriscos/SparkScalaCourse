3c9e876abd274e1bf06c2d28d4ffe46e1d1cc6ae
agustin.delosriscos <agustin.delosriscos@kairosds.com>
2021-08-17 13:44:40 +0200

    Section 4 - Introduction to Spark DS and DF

--- build.sbt
+++ build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.12.14"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.1.2",
+  "org.apache.spark" %% "spark-sql" % "3.1.2",
 )
---
+++ src/main/scala/DataFrames.scala
@@ -0,0 +1,59 @@
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.log4j._
+    
+object DataFrames {
+  
+  case class Person(ID:Int, name:String, age:Int, numFriends:Int)
+  
+  def mapper(line:String): Person = {
+    val fields = line.split(',')  
+    
+    val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
+    return person
+  }
+  
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+    
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+    // Use new SparkSession interface in Spark 2.0
+    val spark = SparkSession
+      .builder
+      .appName("SparkSQL")
+      .master("local[*]")
+      .getOrCreate()
+    
+    // Convert our csv file to a DataSet, using our Person case
+    // class to infer the schema.
+    import spark.implicits._
+    val lines = spark.sparkContext.textFile("./data/fakefriends.csv")
+    val people = lines.map(mapper).toDS()
+    val peopleCached = people.select("name", "age").cache()
+    
+    // There are lots of other ways to make a DataFrame.
+    // For example, spark.read.json("json file path")
+    // or sqlContext.table("Hive table name")
+    
+    println("Here is our inferred schema:")
+    peopleCached.printSchema()
+    
+    println("Let's select the name column:")
+    peopleCached.select("name").show()
+    
+    println("Filter out anyone over 21:")
+    peopleCached.filter(peopleCached("age") < 21).show()
+   
+    println("Group by age:")
+    peopleCached.groupBy("age").count().show()
+    
+    println("Make everyone 10 years older:")
+    peopleCached.select(peopleCached("name"), peopleCached("age") + 10).show()
+
+    peopleCached.unpersist()
+    spark.stop()
+  }
+}
